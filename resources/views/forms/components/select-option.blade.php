8c38872d610620d2a91cca0471ec5cef41bd463e
Adam Weston <adam@awcodes.com>
2025-08-12 12:20:10 -0400

    Fix: failing tests and finalize v4 upgrade

--- resources/views/forms/components/select-option.blade.php
+++ resources/views/forms/components/select-option.blade.php
@@ -1,8 +1,8 @@
-<div class="palette-select-option">
+<div class="palette-select-option flex gap-2 items-center">
     @if ($color['type'] === 'class')
-        <span class="{{ $color['value'] }}"></span>
+        <span class="{{ $color['value'] }} size-4 rounded-full shrink-0" style="line-height: 1; display: block; height: 1rem;"></span>
     @else
-        <span style="background-color: {{ $color['type'] === 'rgb' ? 'rgba(' . $color['value'] . ', 1)' : $color['value'] }};"></span>
+        <span class="size-4 rounded-full shrink-0" style="background-color: {{ $color['type'] === 'rgb' ? 'rgba(' . $color['value'] . ', 1)' : $color['value'] }}; line-height: 1; display: block; height: 1rem;"></span>
     @endif
     <span style="line-height: 1; display: block; height: 1rem;">{{ $color['label'] }}</span>
 </div>
--- src/PaletteServiceProvider.php
+++ src/PaletteServiceProvider.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 namespace Awcodes\Palette;
 
 use Awcodes\Palette\Testing\TestsPalette;
-use Filament\Support\Assets\Css;
-use Filament\Support\Facades\FilamentAsset;
 use Livewire\Features\SupportTesting\Testable;
 use ReflectionException;
 use Spatie\LaravelPackageTools\Package;
@@ -31,13 +29,6 @@ class PaletteServiceProvider extends PackageServiceProvider
      */
     public function packageBooted(): void
     {
-        FilamentAsset::register([
-            Css::make(
-                id: 'palette-select-styles',
-                path: __DIR__.'/../resources/dist/palette-select-styles.css'
-            )->loadedOnRequest(),
-        ], 'awcodes/palette');
-
         Testable::mixin(new TestsPalette);
     }
 }
